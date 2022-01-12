862c97c9ce4123bb32d72070dc790c4875013469
Przemysław Warzecha <przemek.21@op.pl>
2022-01-12 04:48:14 +0100

    added wrapper

--- drivingschool/lib/main.dart
+++ drivingschool/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:drivingschool/services/auth_service.dart';
+import 'package:drivingschool/wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'MainHomePage.dart';
 import 'SignInPage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,8 +18,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MainHomePage(),
+    return MultiProvider(
+        providers: [
+          Provider<AuthService>(create: (_) => AuthService(),
+          ),
+        ],
+      child: MaterialApp(
+        home: Wrapper(),
+      )
+
     );
   }
 }
--- drivingschool/lib/wrapper.dart
+++ drivingschool/lib/wrapper.dart
@@ -1,10 +1,27 @@
+import 'package:drivingschool/LoggedHomePage.dart';
+import 'package:drivingschool/SignInPage.dart';
+import 'package:drivingschool/services/auth_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'models/user_model.dart';
 
 class Wrapper extends StatelessWidget{
   @override
   Widget build(BuildContext context)
   {
-    return Container();
+    final authService = Provider.of<AuthService>(context);
+    return StreamBuilder<User?>(
+        stream: authService.user,
+        builder: (_, AsyncSnapshot<User?> snapshot){
+          if (snapshot.connectionState == ConnectionState.active){
+            final User? user = snapshot.data;
+            return user == null ? SignInPage() : LoggedHomePage(); }
+          else {
+            return Scaffold(body: Center (child: CircularProgressIndicator(),))
+          }
+          }
+        });
   }
 }
