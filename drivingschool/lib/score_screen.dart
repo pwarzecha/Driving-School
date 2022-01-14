a3b134aa17a070476f3ee19656c7066390552ec1
Przemysław Warzecha <przemek.21@op.pl>
2022-01-14 06:06:15 +0100

    updated score screen, back button

--- drivingschool/lib/score_screen.dart
+++ drivingschool/lib/score_screen.dart
@@ -1,3 +1,4 @@
+import 'package:drivingschool/main_home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:drivingschool/constants.dart';
@@ -31,6 +32,40 @@ class ScoreScreen extends StatelessWidget {
                     .copyWith(color: kSecondaryColor),
               ),
               Spacer(flex: 3),
+
+              ElevatedButton(
+                style:ButtonStyle(
+                    backgroundColor:  MaterialStateProperty.all<Color>(const Color(0xff252427)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(28.0),
+                        )
+                    )
+                ),
+
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainHomePage()),
+                  );
+
+                },
+                child: Container(
+                  child: const Text(
+                    'Powrot   ',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 18,
+                      color: Color(0xffffffff),
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+
+                ),
+              ),
+
+
             ],
           )
         ],
@@ -38,3 +73,6 @@ class ScoreScreen extends StatelessWidget {
     );
   }
 }
+
+
+
