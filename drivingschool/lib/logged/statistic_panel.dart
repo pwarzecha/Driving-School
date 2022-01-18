01960a0d4b61bd9eb08e3d3e740813ef028cc3ab
Przemysław Warzecha <przemek.21@op.pl>
2022-01-18 19:51:08 +0100

    statistics panel

--- drivingschool/lib/logged/statistic_panel.dart
+++ drivingschool/lib/logged/statistic_panel.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+//import 'package:charts_flutter/flutter.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'dart:math';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 
+class passingExams {
+  passingExams(this.isPassed, this.examCount);
+  final String isPassed;
+  final int examCount;
 
 
-class StatisticPanel extends StatelessWidget {
+}
+
+class StatisticPanel extends StatefulWidget {
+  @override
+  State<StatisticPanel> createState() => _StatisticPanelState();
+}
+
+class _StatisticPanelState extends State<StatisticPanel> {
+  List<passingExams> _chartData;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    super.initState();
+  }
+
   //final Stream<QuerySnapshot> users = FirebaseFirestore.instance.collection('users').snapshots();
   @override
   Widget build(BuildContext context) {
@@ -14,9 +36,35 @@ class StatisticPanel extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                'Dane statystyczne',
+                '\n\nDane statystyczne\n',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)
+
               ),
+              Text(
+                  'Username',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)
+
+              ),
+              Text(
+                  'Liczba niezdanych egzaminow: 3',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)
+
+              ),
+              Text(
+                  'Liczba zdanych egzaminow: 1',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)
+
+              ),
+              SfCircularChart(
+                legend: Legend(isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
+                series: <CircularSeries> [DoughnutSeries<passingExams, String>(
+                dataSource: _chartData,
+                xValueMapper: (passingExams data,_) => data.isPassed,
+                yValueMapper: (passingExams data,_) => data.examCount,
+                dataLabelSettings: DataLabelSettings(isVisible: true)
+              )],)
+
+
               // Container(
               //   height: 250,
               //   padding: const EdgeInsets.symmetric(vertical: 20),
@@ -49,6 +97,14 @@ class StatisticPanel extends StatelessWidget {
         ),
       )
     );
+  }
+
+  List<passingExams> getChartData(){
+    final List<passingExams> chartData = [
+      passingExams("Pozytywny", 1),
+      passingExams("Negatywny", 3),
+    ];
+    return chartData;
   }
 }
 
